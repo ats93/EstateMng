a1dce42956d099d431a6c117c9dcadf38e5f5038
Arseniy <arseniy30@gmail.com>
2020-07-04 15:35:06 +0200

    Working entities. Need to fix mapStruct

---
+++ src/main/java/com/arseny/estatemng/entities/AreaId.java
@@ -0,0 +1,4 @@
+package com.arseny.estatemng.entities;
+
+public class AreaId {
+}
---
+++ src/main/java/com/arseny/estatemng/mapper/MapperWrapper.java
@@ -0,0 +1,9 @@
+package com.arseny.estatemng.service;
+
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface MapperWrapper {
+
+    @
+}
